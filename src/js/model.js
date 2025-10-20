6bdb4a071da39079fe238ee5b035cdc9175391e3
Evan Lackey <elackey@sandiego.edu>
2025-10-19 23:47:56 -0700

    model.js : import JSON added to model.js

--- src/js/model.js
+++ src/js/model.js
@@ -49,7 +49,12 @@ export class Model {
         return this.messages.find(msg => msg.id === parseInt(id));
     }
 
-
+    getStats() {
+        return{
+            count: this.messages.length,
+            lastSaved: this.lastSaved
+        };
+    }
 
     // UPDATE allows user to update their message
     updateMessage(id, newText) {
@@ -64,5 +69,47 @@ export class Model {
         // let the user know
         document.dispatchEvent(new CustomEvent('messagesChanged', { detail: { messages: this.getMessages() } }));
         return msg;
+    }
+
+    deleteMessage(id) {
+        this.messages = this.messages.filter(msg => msg.id === parseInt(id));
+        this.saveToStorage();
+        this.lastSaved = new Date().toLocaleString();
+        localStorage.setItem('lastSaved', this.lastSaved);
+        document.dispatchEvent(new CustomEvent('deleteMessage', { detail: { messages: this.getMessages() } }));
+    }
+
+    clearChat() {
+        this.messages = [];
+        this.saveToStorage();
+        this.lastSaved = 'Never';
+        localStorage.setItem('lastSaved', this.lastSaved);
+        document.dispatchEvent(new CustomEvent('clearChat', { detail: { messages: this.getMessages() } }));
+    }
+
+    exportJSON() {
+        return JSON.stringify(this.messages, null, 2);
+    }
+
+    importJSON(jsonString) {
+        try {
+            const imported = JSON.parse(jsonString);
+            if (!Array.isArray(imported)) throw new Error('Invalid format');
+            this.messages = imported.map(msg => ({
+                ...msg,
+                id: msg.id || Date.now(), // Ensure unique ID
+                timestamp: msg.timestamp || new Date().toISOString(),
+                edited: msg.edited || false
+            }));
+            this.saveToStorage();
+            this.lastSaved = new Date().toLocaleString();
+            localStorage.setItem('lastSaved', this.lastSaved);
+            // Notify observers
+            document.dispatchEvent(new CustomEvent('messagesChanged', { detail: { messages: this.getMessages() } }));
+            return true;
+        } catch (error) {
+            console.error('Import error:', error);
+            return false;
+        }
     }
 }
