7eb4a52d30b140ef9f1e41d7dea7d991728f3def
Evan Lackey <elackey@sandiego.edu>
2025-10-20 18:42:56 -0700

    View rough draft finished moving on to controller

--- src/js/view.js
+++ src/js/view.js
@@ -43,4 +43,33 @@ export class view {
     showConfirmation(message) {
         return confirm(message);
     }
+
+    downloadJSON(filename, data) {
+        const blob = new Blob([data], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    getFileContent(callback) {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+        input.onchange = (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (ev) => callback(ev.target.result);
+                reader.readAsText(file);
+            }
+        };
+        input.click();
+    }
+
+    getMessageIdForEvent(event) {
+        return event.target.dataset.messageId;
+    }
 }
